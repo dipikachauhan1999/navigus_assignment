a0fcbfefdebf8ff3b7ce95f3347e7a624786d079
Dipika Chauhan <deepikachauhan1007@gmail.com>
2021-09-23 13:02:51 +0530

    Migrating to MongoAtlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/navigus',{
-useNewURLParser:true,
+const db='mongodb+srv://dipika:Deeputanu%4019@cluster0.yspis.mongodb.net/navigus?retryWrites=true&w=majority'
 
+mongoose.connect(db,{
+    useNewURLParser:true,
+}).then(()=>{
+    console.log('Connected to database');
+}).catch((e)=>{
+    console.log(e);
 })
 
 
