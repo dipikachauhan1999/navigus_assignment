72ec39c79ce8c74c1b3e3079c39464e631d7f5ac
Dipika Chauhan <deepikachauhan1007@gmail.com>
2021-09-23 14:52:51 +0530

    Adding to UI

--- src/index.js
+++ src/index.js
@@ -15,9 +15,19 @@ app.use(courseRouter)
 app.use(questionRouter)
 app.use(quizRouter)
 
-app.get('/',(req,res)=>{
-    res.render("<h1>hello</h1>")
-})
+app.get('/',  (req, res)=> {
+    res.header('Content-Type', 'text/html').send(`<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Navigus</title>
+    </head>
+    <body>
+        <h1>Welcome to My Navigus Assignment</h1>
+    </body>
+    </html>`);
+ });
 
 app.listen(port,()=>{
     console.log('Server is running @'+ port);
